80c9211c562791b6181fdcaf9964da34bb0bd027
Thiago Fernando Rech <thiagorech.1997@gmail.com>
2022-12-05 09:53:03 -0300

    style

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 export default function Login() {
     return (
-        <div className="">
-            <h1>Login</h1>
-            <input type="email" placeholder='email'/>
-            <input type="password" placeholder='password'/>
-            <button type="submit">Enviar</button>
-        </div>
+        <div className='flex justify-center items-center h-screen'>
+            <div className="flex flex-col">
+                <h1 className='text-3xl text-center'>Login</h1>
+                <input className='text-2xl' type="email" placeholder='email'/>
+                <input className='text-2xl' type="password" placeholder='password'/>
+                <button className='bg-gray-400 text-2xl rounded-lg' type="submit">Enviar</button>
+            </div>
+        </div>    
     )
 }
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 export default function Users() {
     return (
-        <div className='flex justify-center'>
-            <h1>Cadastro</h1>
-            <input type="text" placeholder='Nome' required={true}/>
-            <input type="email" placeholder='Email' />
-            <input type="password" placeholder='Senha' />
-            <button type='submit'>Cadastrar</button>
+        <div className='flex justify-center items-center h-screen'>
+            <div className='flex flex-col'>
+                <h1 className='text-5xl text-center'>Cadastro Usuario</h1>
+                <input className='text-2xl' type="text" placeholder='Nome' required={true}/>
+                <input className='text-2xl' type="email" placeholder='Email' />
+                <input className='text-2xl' type="password" placeholder='Senha' />
+                <button className='bg-gray-400 text-2xl rounded-lg' type='submit'>Cadastrar</button>
+            </div>
         </div>
     )
 }
